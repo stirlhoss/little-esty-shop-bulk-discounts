c4eb2559bd46be3984cb95197e695ed890ab0790
Stirling Hostetter <stirhoss@icloud.com>
2022-06-02 11:53:37 -0500

    Fix: spacing

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Transaction, type: :model do
   end
 
   describe 'validations' do
-      it { should validate_presence_of :invoice_id }
-      it { should validate_presence_of :credit_card_number }
-      it { should validate_presence_of :result }
+    it { should validate_presence_of :invoice_id }
+    it { should validate_presence_of :credit_card_number }
+    it { should validate_presence_of :result }
   end
 end
