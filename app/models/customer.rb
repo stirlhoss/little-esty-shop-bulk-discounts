6acdb73bae93fc98c35dcf29122dab7ff5bb3a91
Stirling Hostetter <stirhoss@icloud.com>
2022-06-02 20:16:22 -0500

    Feat: working on implementing top_customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,18 @@
 class Customer < ApplicationRecord
+  validates_presence_of :first_name,
+                        :last_name
+
   has_many :invoices
   has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.top_customers
+    joins(invoices: :transactions)
+      .where('transactions.result = ?', 'success')
+      .group('customers.id')
+      .select('customers.*')
+      .order(count: :desc)
+      .limit(5)
+      # .count
+  end
 end
