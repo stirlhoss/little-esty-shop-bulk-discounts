0ed28c045059618a38f5eddf990c55d8df045f9d
Bryce Wein <bryce.wein@gmail.com>
2022-06-02 21:41:12 -0400

    Refactor: move instance methods into place

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,72 +12,75 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
-
-  before :each do
-    # InvoiceItem.destroy_all
-    # Transaction.destroy_all
-    # Invoice.destroy_all
-    # Customer.destroy_all
-    # Merchant.destroy_all
-    @merchant = Merchant.create!(name: 'Saba')
-    @item_1 = @merchant.items.create!(name: 'Pencil', unit_price: 4, description: 'Writes things.')
-    @item_2 = @merchant.items.create!(name: 'Pen', unit_price: 5, description: 'Writes things, but dark.')
-    @item_3 = @merchant.items.create!(name: 'Marker', unit_price: 6, description: 'Writes things, but dark, and thicc.')
-
-    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
-    @invoice_1 = @customer_1.invoices.create!(status: "completed")
-    @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 0)
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249632', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249631', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249633', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
-
-    @customer_2 = Customer.create!(first_name: 'Osinski', last_name: 'Cecelia')
-    @invoice_4 = @customer_2.invoices.create!(status: "completed")
-    @item_1.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 3, unit_price: 4, status: 1)
-    @invoice_4.transactions.create!(credit_card_number: '5654405418249632', result: 'success')
-    @invoice_4.transactions.create!(credit_card_number: '5654405418249631', result: 'success')
-    @invoice_4.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
-    @invoice_4.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
-
-    @customer_3 = Customer.create!(first_name: 'Toy', last_name: 'Mariah')
-    @invoice_7 = @customer_3.invoices.create!(status: "completed")
-    @item_2.invoice_items.create!(invoice_id: @invoice_7.id, quantity: 4, unit_price: 5, status: 0)
-    @invoice_7.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
-    @invoice_7.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
-    @invoice_7.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
-
-    @customer_4 = Customer.create!(first_name: 'Joy', last_name: 'Braun')
-    @invoice_9 = @customer_4.invoices.create!(status: "completed")
-    @item_2.invoice_items.create!(invoice_id: @invoice_9.id, quantity: 4, unit_price: 5, status: 1)
-    @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-
-    @customer_5 = Customer.create!(first_name: 'Eileen', last_name: 'Gerlach')
-    @invoice_11 = @customer_5.invoices.create!(status: "completed")
-    @item_2.invoice_items.create!(invoice_id: @invoice_11.id, quantity: 4, unit_price: 5, status: 0)
-    @invoice_11.transactions.create!(credit_card_number: '6654405418249643', result: 'success')
-
-    @customer_6 = Customer.create!(first_name: 'Smark', last_name: 'Mrains')
-    @invoice_13 = @customer_6.invoices.create!(status: "in progress")
-    @item_2.invoice_items.create!(invoice_id: @invoice_11.id, quantity: 4, unit_price: 5, status: 'pending')
-    @invoice_13.transactions.create!(credit_card_number: '6654405418249644', result: 'failed')
-  end
-
-  it "#fave_customers" do
-    actual = @merchant.fave_customers.map do |customer|
-      customer[:first_name]
-    end
-    expect(actual).to eq([@customer_4.first_name, @customer_1.first_name, @customer_2.first_name, @customer_3.first_name, @customer_5.first_name])
-  end
-
-  # it "#unshipped_items" do
-  #   expect(@merchant.unshipped_items).to eq([])
+  # describe 'sabas methods' do
+  #   before :each do
+  #     # InvoiceItem.destroy_all
+  #     # Transaction.destroy_all
+  #     # Invoice.destroy_all
+  #     # Customer.destroy_all
+  #     # Merchant.destroy_all
+  #     @merchant = Merchant.create!(name: 'Saba')
+  #     @item_1 = @merchant.items.create!(name: 'Pencil', unit_price: 4, description: 'Writes things.')
+  #     @item_2 = @merchant.items.create!(name: 'Pen', unit_price: 5, description: 'Writes things, but dark.')
+  #     @item_3 = @merchant.items.create!(name: 'Marker', unit_price: 6,
+  #                                       description: 'Writes things, but dark, and thicc.')
+  #
+  #     @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
+  #     @invoice_1 = @customer_1.invoices.create!(status: 'completed')
+  #     @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 4, status: 0)
+  #     @invoice_1.transactions.create!(credit_card_number: '4654405418249632', result: 'success')
+  #     @invoice_1.transactions.create!(credit_card_number: '4654405418249631', result: 'success')
+  #     @invoice_1.transactions.create!(credit_card_number: '4654405418249633', result: 'success')
+  #     @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
+  #     @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
+  #
+  #     @customer_2 = Customer.create!(first_name: 'Osinski', last_name: 'Cecelia')
+  #     @invoice_4 = @customer_2.invoices.create!(status: 'completed')
+  #     @item_1.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 3, unit_price: 4, status: 1)
+  #     @invoice_4.transactions.create!(credit_card_number: '5654405418249632', result: 'success')
+  #     @invoice_4.transactions.create!(credit_card_number: '5654405418249631', result: 'success')
+  #     @invoice_4.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
+  #     @invoice_4.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
+  #
+  #     @customer_3 = Customer.create!(first_name: 'Toy', last_name: 'Mariah')
+  #     @invoice_7 = @customer_3.invoices.create!(status: 'completed')
+  #     @item_2.invoice_items.create!(invoice_id: @invoice_7.id, quantity: 4, unit_price: 5, status: 0)
+  #     @invoice_7.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
+  #     @invoice_7.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
+  #     @invoice_7.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
+  #
+  #     @customer_4 = Customer.create!(first_name: 'Joy', last_name: 'Braun')
+  #     @invoice_9 = @customer_4.invoices.create!(status: 'completed')
+  #     @item_2.invoice_items.create!(invoice_id: @invoice_9.id, quantity: 4, unit_price: 5, status: 1)
+  #     @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
+  #     @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
+  #     @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
+  #     @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
+  #     @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
+  #     @invoice_9.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
+  #
+  #     @customer_5 = Customer.create!(first_name: 'Eileen', last_name: 'Gerlach')
+  #     @invoice_11 = @customer_5.invoices.create!(status: 'completed')
+  #     @item_2.invoice_items.create!(invoice_id: @invoice_11.id, quantity: 4, unit_price: 5, status: 0)
+  #     @invoice_11.transactions.create!(credit_card_number: '6654405418249643', result: 'success')
+  #
+  #     @customer_6 = Customer.create!(first_name: 'Smark', last_name: 'Mrains')
+  #     @invoice_13 = @customer_6.invoices.create!(status: 'in progress')
+  #     @item_2.invoice_items.create!(invoice_id: @invoice_11.id, quantity: 4, unit_price: 5, status: 'pending')
+  #     @invoice_13.transactions.create!(credit_card_number: '6654405418249644', result: 'failed')
+  #   end
+  #
+  #   it '#fave_customers' do
+  #     actual = @merchant.fave_customers.map do |customer|
+  #       customer[:first_name]
+  #     end
+  #     expect(actual).to eq([@customer_4.first_name, @customer_1.first_name, @customer_2.first_name,
+  #                           @customer_3.first_name, @customer_5.first_name])
+  #   end
+  #
+  #   # it "#unshipped_items" do
+  #   #   expect(@merchant.unshipped_items).to eq([])
+  #   # end
   # end
 
   describe 'class methods' do
@@ -127,7 +130,7 @@ RSpec.describe Merchant, type: :model do
       @t5 = Transaction.create!(credit_card_number: 102_938, result: 1, invoice_id: @i6.id)
       @t6 = Transaction.create!(credit_card_number: 102_938, result: 1, invoice_id: @i1.id)
     end
-    
+
     it '#top_five_merchants' do
       expect(Merchant.top_five_merchants.to_a).to eq([@m1, @m3, @m2])
     end
@@ -173,16 +176,27 @@ RSpec.describe Merchant, type: :model do
       @ii_6 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_3.id, quantity: 7, unit_price: 5, status: 2)
       @ii_7 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
 
-      @t1 = Transaction.create!(credit_card_number: 203_942, result: 1, invoice_id: @i1.id)
-      @t2 = Transaction.create!(credit_card_number: 230_948, result: 1, invoice_id: @i2.id)
-      @t3 = Transaction.create!(credit_card_number: 234_092, result: 1, invoice_id: @i3.id)
-      @t4 = Transaction.create!(credit_card_number: 230_429, result: 1, invoice_id: @i5.id)
-      @t5 = Transaction.create!(credit_card_number: 102_938, result: 1, invoice_id: @i6.id)
-      @t6 = Transaction.create!(credit_card_number: 102_938, result: 1, invoice_id: @i1.id)
+      @t1 = Transaction.create!(credit_card_number: 203_942, result: 0, invoice_id: @i1.id)
+      @t2 = Transaction.create!(credit_card_number: 230_948, result: 0, invoice_id: @i2.id)
+      @t3 = Transaction.create!(credit_card_number: 234_092, result: 0, invoice_id: @i3.id)
+      @t4 = Transaction.create!(credit_card_number: 230_429, result: 0, invoice_id: @i5.id)
+      @t5 = Transaction.create!(credit_card_number: 102_938, result: 0, invoice_id: @i6.id)
+      @t6 = Transaction.create!(credit_card_number: 102_938, result: 0, invoice_id: @i1.id)
     end
-    
+
     it '#top_day' do
       expect(@m3.best_day.created_at).to eq(@i3.created_at)
     end
+
+    it '#fave_customers' do
+      actual = @m3.fave_customers.map do |customer|
+        customer[:first_name]
+      end
+      expect(actual).to eq([@c2.first_name, @c1.first_name])
+    end
+
+    # it '#unshipped_items' do
+    #   expect(@m1.unshipped_items).to eq([])
+    # end
   end
 end
