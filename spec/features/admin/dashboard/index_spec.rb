ec6dbb3162545f030dc86baa58cdc3643b79c2ff
Stirling Hostetter <stirhoss@icloud.com>
2022-06-04 16:27:06 -0500

    Test: Manipulated test for incomplete invoices

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -42,14 +42,14 @@ RSpec.describe 'admin dashboard' do
       @invoice_1 = @customer_1.invoices.create!(status: 'completed')
       @invoice_2 = @customer_2.invoices.create!(status: 'completed')
       @invoice_3 = @customer_3.invoices.create!(status: 'completed')
-      @invoice_4 = @customer_4.invoices.create!(status: 'in progress')
-      @item.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 400, status: 'packaged',
+      @invoice_4 = @customer_4.invoices.create!(status: 'completed')
+      @ii_1 = @item.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 400, status: 'shipped',
                                   created_at: Time.parse('2012-03-27 14:54:09 UTC'))
-      @item.invoice_items.create!(invoice_id: @invoice_2.id, quantity: 5, unit_price: 400, status: 'packaged',
+      @ii_2 = @item.invoice_items.create!(invoice_id: @invoice_2.id, quantity: 5, unit_price: 400, status: 'packaged',
                                   created_at: Time.parse('2012-03-27 14:54:09 UTC'))
-      @item.invoice_items.create!(invoice_id: @invoice_3.id, quantity: 1, unit_price: 400, status: 'packaged',
+      @ii_3 = @item.invoice_items.create!(invoice_id: @invoice_3.id, quantity: 1, unit_price: 400, status: 'shipped',
                                   created_at: Time.parse('2012-03-27 14:54:09 UTC'))
-      @item.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 7, unit_price: 400, status: 'packaged',
+      @ii_4 = @item.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 7, unit_price: 400, status: 'packaged',
                                   created_at: Time.parse('2012-03-27 14:54:09 UTC'))
       @invoice_1.transactions.create!(credit_card_number: '4654405418249632', result: 'success')
       @invoice_1.transactions.create!(credit_card_number: '4654405418249631', result: 'success')
@@ -63,7 +63,7 @@ RSpec.describe 'admin dashboard' do
       @invoice_4.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
     end
 
-    xit 'shows the top 5 customers and their order count' do
+    it 'shows the top 5 customers and their order count' do
       visit admin_dashboard_path
 
       expect(page).to have_content("#{@customer_1.first_name} #{@customer_1.last_name} 4")
@@ -74,13 +74,15 @@ RSpec.describe 'admin dashboard' do
 
     it 'shows a section of incomplete invoices' do
       visit admin_dashboard_path
-      within '#incomplete invoices' do
-        expect(page).to have_link(@invoice_4.id.to_s)
+      within '#incomplete_invoices' do
+        expect(page).to have_content(@ii_2.id.to_s)
+        expect(page).to have_content(@ii_4.id.to_s)
 
-        click_link(@invoice_4.id.to_s)
-        # admin invoice show path does not exist yet
-        expect(current_path).to eq(admin_invoice_show_path)
+        expect(page).to have_link(@ii_2.id.to_s)
+
+        click_link(@ii_2.id.to_s)
       end
+        expect(current_path).to eq(admin_invoices_path(@ii_2.id))
     end
   end
 end
