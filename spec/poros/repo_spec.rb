56f21809f7172beb04b21bcc968f9a5a6f0abea2
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-06-07 07:53:19 -0400

    test: add test for poros

---
+++ spec/poros/repo_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Repository do
+  it "exists and has attributes" do
+    data = {name: "test_repo"}
+    repo = Repository.new(data)
+    expect(repo).to be_a(Repository)
+    expect(repo.name).to eq(data[:name])
+  end
+end
